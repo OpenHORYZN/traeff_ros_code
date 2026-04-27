d247b13db9404f0577ddcae305fa4fdf96d46e3a
VSA <vicente.serna@horyzn.org>
2026-04-27 15:15:40 +0200

    Added realsense launch

--- ros_src/mission_control/launch/main_mission.launch.py
+++ ros_src/mission_control/launch/main_mission.launch.py
@@ -1,36 +1,52 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
+    realsense_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            FindPackageShare('realsense2_camera'), '/launch/rs_launch.py'
+        ]),
+        launch_arguments={
+            'depth_module.enable':     'false',
+            'pointcloud.enable':       'false',
+            'rgb_camera.color_width':  '1920',
+            'rgb_camera.color_height': '1080',
+            'rgb_camera.color_fps':    '30',
+        }.items()
+    )
+
+    mission_control_node = Node(
+        package='mission_control',
+        executable='main_mission',
+        name='main_mission',
+        output='screen',
+        parameters=[{
+            # Topics
+            "odometry_topic":    "/mavros/local_position/odom",
+            "image_topic":       "/camera/camera/color/image_raw",
+            "camera_info_topic": "/camera/camera/color/camera_info",
+            "twist_topic":       "/mavros/setpoint_attitude/cmd_vel",
+            # Vision — marker_size in metres
+            "marker_size": 0.15,
+            # PID X
+            "kp_x": 0.2,
+            "ki_x": 0.01,
+            "kd_x": 0.01,
+            # PID Y
+            "kp_y": 0.2,
+            "ki_y": 0.01,
+            "kd_y": 0.01,
+            # Flight
+            "down_speed":   -0.2,
+            "flight_height": 2.3,
+        }]
+    )
+
     return LaunchDescription([
-        Node(
-            package='mission_control',
-            executable='main_mission',
-            name='main_mission',
-            output='screen',
-            parameters=[{
-                # Topics
-                "odometry_topic":    "/mavros/local_position/odom",
-                "image_topic":       "/camera/camera/color/image_raw",
-                "camera_info_topic": "/camera/camera/color/camera_info",
-                "twist_topic":       "/mavros/setpoint_attitude/cmd_vel",
-
-                # Vision — marker_size in metres
-                "marker_size": 0.15,
-
-                # PID X
-                "kp_x": 0.2,
-                "ki_x": 0.01,
-                "kd_x": 0.01,
-
-                # PID Y
-                "kp_y": 0.2,
-                "ki_y": 0.01,
-                "kd_y": 0.01,
-
-                # Flight
-                "down_speed":    -0.2,
-                "flight_height":  2.3,
-            }]
-        )
+        realsense_launch,
+        mission_control_node,
     ])
